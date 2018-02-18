f9f0cce52b673f642cd2566ca99ffe9535d66a8a
Paweł Gardziel <gardzielpawel@gmail.com>
2018-02-18 16:26:26 +0100

    modul_14.6 wersja 1

--- script.js
+++ script.js
@@ -1,2 +1,62 @@
-var app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('button',{onClick: this.increment}),
+            React.createElement('button',{onClick: this.decrement}),
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        );
+    }
+});
+
+var element =
+    React.createElement('div', {},
+        React.createElement(Counter, {}, ),
+        React.createElement(Counter, {}, ),
+        React.createElement(Counter, {}, ) 
+    );
+
+// componentWillMount(); {
+//     console.log('WillMount');
+// }
+// componentDidMount(); {
+//     console.log('DidMount');
+// }
+// componentWillReceiveProps(); {
+//     console.log('WillReceivePropst');
+// }
+// shouldComponentUpdate(); {
+//     console.log('componentUpdate')
+//         // return true;
+// }
+// componentWillUpdate(); {
+//     console.log('WillUpdate');
+// }
+// componentDidUpdate(); {
+//     console.log('DidUpdate');
+// }
+// componentWillUnmountn(); {
+//     console.log('WillUnmount');
+// }
+
+
+
+
+ReactDOM.render(element, document.getElementById('app'));
