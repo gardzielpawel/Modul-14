64d9a47e94350a948dc86b8fc2d7d486e6a2ca05
Paweł Gardziel <gardzielpawel@gmail.com>
2018-02-19 07:58:20 +0100

    moduł 14.4 wersja 2

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// var movies = [
-//     {
-//         id: 1,
-//         title: 'Harry Potter',
-//         desc: 'Film o czarodzieju',
-//         actor: 'Daniel Radcliffe',
-//         poster: 'http://ocdn.eu/images/pulscms/M2Q7MDMsMCwyZDAsMSwx/21f973867807dbb87dda1f84a988902b.jpg'
-//     },
-//     {
-//         id: 2,
-//         title: 'Król Lew',
-//         desc: 'Film o królu sawanny',
-//         actor: 'Cartoon animals',
-//         poster: 'http://1.fwcdn.pl/po/68/78/6878/6927221.3.jpg'
-//     },
-//     {
-//         id: 3,
-//         title: 'Ojciec chrzestny',
-//         desc: 'Film o mafii',
-//         actor: 'Marlon Brando',
-//         poster: 'https://www.filmfan.pl/obrazki/plakaty/1972/ojciec-chrzestny-1972_20111121183135.jpg'
-//     },
-//     {
-//         id: 4,
-//         title: 'Forrest Gump',
-//         desc: 'Film o jakimś kolesiu',
-//         actor: 'Tom Hanks',
-//         poster: 'https://www.galeriaplakatu.com/img/product_media/190695_1020_A.jpg'
-//     }
-//   ];
+var movies = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'Film o czarodzieju',
+        actor: 'Daniel Radcliffe',
+        poster: 'http://ocdn.eu/images/pulscms/M2Q7MDMsMCwyZDAsMSwx/21f973867807dbb87dda1f84a988902b.jpg'
+    },
+    {
+        id: 2,
+        title: 'Król Lew',
+        desc: 'Film o królu sawanny',
+        actor: 'Cartoon animals',
+        poster: 'http://1.fwcdn.pl/po/68/78/6878/6927221.3.jpg'
+    },
+    {
+        id: 3,
+        title: 'Ojciec chrzestny',
+        desc: 'Film o mafii',
+        actor: 'Marlon Brando',
+        poster: 'https://www.filmfan.pl/obrazki/plakaty/1972/ojciec-chrzestny-1972_20111121183135.jpg'
+    },
+    {
+        id: 4,
+        title: 'Forrest Gump',
+        desc: 'Film o jakimś kolesiu',
+        actor: 'Tom Hanks',
+        poster: 'https://www.galeriaplakatu.com/img/product_media/190695_1020_A.jpg'
+    }
+  ];
 
 
 
@@ -48,23 +48,87 @@
 //     React.createElement('ul', {}, moviesElements)
 //   );
 
-//   ReactDOM.render(element, document.getElementById('app'));
 
 
-//   var Movie = React.createClass({
-//     propTypes: {
-//         movie: React.PropTypes.object.isRequired,
-//       },
 
-//         render: function() {
-//             return (
-//          //         React.createElement('li', {key: movie.id},
-            //         React.createElement('h2', {}, movie.title),
-            //         React.createElement('p', {}, movie.desc),
-            //         React.createElement('p', {}, movie.actor),
-            //         React.createElement('img', {src: movie.poster})
-        //       );
-//             )
-//         )}
-//     })      
+  var Movie = React.createClass({
+    propTypes: {
+        movie: React.PropTypes.object.isRequired,
+      },
 
+        render: function() {
+            return (
+                 React.createElement('li', {key: movie.id},
+                    React.createElement('h2', {}, movie.title),
+                    React.createElement('p', {}, movie.desc),
+                    React.createElement('p', {}, movie.actor),
+                    React.createElement('img', {src: movie.poster})
+              )
+            )
+        )}
+    })      
+
+var MovieTitle = React.createClass({
+    propTypes: {
+        title: React.PropTypes.object.isRequired,
+    },
+    
+    render: function () {
+        return (
+            React.createElement('h2', {}, movie.title)
+        )
+    }
+});
+
+
+var MovieDesc = React.createClass({
+    propTypes: {
+        desc: React.PropTypes.object.isRequired,
+    },
+    
+    render: function () {
+        return (
+            React.createElement('p', {}, movie.desc)
+        )
+    }
+});
+
+var MovieActor = React.createClass({
+    propTypes: {
+        actor: React.PropTypes.object.isRequired,
+    },
+    
+    render: function () {
+        return (
+            React.createElement('p', {}, movie.actor)
+        )
+    }
+});
+
+var MoviePoster = React.createClass({
+    propTypes: {
+        poster: React.PropTypes.object.isRequired,
+    },
+    
+    render: function () {
+        return (
+            React.createElement('img', {src: movie.poster})
+        )
+    }
+});
+
+var MovieList = React.createClass ({
+    propTypes: {
+        list: React.PropTypes.object.isRequired,
+    },
+
+    render: function () {
+        return (
+            React.createElement('li', {key: movie.id})
+        )
+    }
+})
+
+
+
+  ReactDOM.render(element, document.getElementById('app'));
